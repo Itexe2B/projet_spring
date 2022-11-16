e517d7b8e049a553efe61d4e555ecdbc037b0650
Diego <48367473+Itexe2B@users.noreply.github.com>
2022-11-16 17:22:03 +0100

    Eureka Client/Server

--- my-finder/src/main/java/com/example/myfinder/MyFinderApplication.java
+++ my-finder/src/main/java/com/example/myfinder/MyFinderApplication.java
@@ -2,12 +2,14 @@ package com.example.myfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableHystrix
 @EnableHystrixDashboard
 @SpringBootApplication
+@EnableEurekaClient
 public class MyFinderApplication {
 
     public static void main(String[] args) {
